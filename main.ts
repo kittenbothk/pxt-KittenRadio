95f9b2c1a7f143849f675996ad531221dd4796f9
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2020-10-20 14:55:20 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,213 @@
-basic.forever(function () {
-	
-})
+/*
+KittenbotHK
+Microbit powerbrick extension board
+
+MicroPython ESP8266/ESP32 driver for TEA5767 FM radio module:
+https://github.com/alankrantas/micropython-TEA5767
+
+TEA5767 Datasheet:
+https://www.sparkfun.com/datasheets/Wireless/General/TEA5767.pdf
+*/
+
+
+//% color="#13c2c2" weight=10 icon="\uf0e7"
+//% groups='["TEA5767"]'
+namespace TEA5767 {
+  const range_US = [87.5, 108.0]
+  const range_JP = [76.0, 91.0]
+
+  const addr = 0x60
+  let frequency = 88.1
+  let band = 'US'
+  let standby_mode = false
+  let mute_mode = false
+  let soft_mute_mode = true
+  let search_mode = false
+  let search_direction = 1
+  let search_adc_level = 7
+  let stereo_mode = true
+  let noise_cancel = true
+  let high_cut_mode = true
+  let is_ready = false
+  let is_stereo = false
+  let signal_adc_level = 0
+  let buf
+//% blockID=radio_init block="Initialization"
+//% weight=60
+//% group="TEA5767"
+  export function init() {
+    //oaknaso=0
+    frequency = 88.1
+    band = 'US'
+    standby_mode = false
+    mute_mode = false
+    soft_mute_mode = true
+    search_mode = false
+    search_direction = 1
+    search_adc_level = 7
+    stereo_mode = true
+    noise_cancel = true
+    high_cut_mode = true
+    is_ready = false
+    is_stereo = false
+    signal_adc_level = 0
+    buf
+    update()
+  }
+  //% blockID=radio_setFreq block="set frequency|%freq"
+  //% weight=60
+  //% group="TEA5767"
+  export function set_frequency(freq: number) {
+    frequency = freq
+    update()
+  }
+  //% blockID=radio_changeFreq block="change frequency|%change"
+  //% weight=60
+  //% group="TEA5767"
+  export function change_frequency(change: number) {
+    frequency += change
+    if (change >= 0) {
+      search_direction = 1
+    } else {
+      search_direction = 0
+    }
+    update()
+  }
+  //% blockID=radio_search block="search|%mode|direction %dir|adc %adc"
+  //% weight=60
+  //% group="TEA5767"
+  export function search(mode: boolean, dir: number, adc: number) {
+    search_mode = mode
+    search_direction = dir
+    if (adc == 10 || adc == 7 || adc == 5 || adc == 0) {
+      search_adc_level = adc
+    } else {
+      search_adc_level = 7
+    }
+    update()
+  }
+  //% blockID=radio_mute block="mute|%mode"
+  //% weight=60
+  //% group="TEA5767"
+  export function mute(mode: boolean) {
+    mute_mode = mode
+    update()
+  }
+  //% blockID=radio_standby block="standby|%mode"
+  //% weight=60
+  //% group="TEA5767"
+  export function standby(mode: boolean) {
+    standby_mode = mode
+    update()
+  }
+  function read() {
+    //reg: number
+    //pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE)
+    //let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE)
+    let temp=pins.i2cReadBuffer(0x60,5)
+    let buf0=String(temp[0])
+    let buf1=String(temp[1])
+    let buf01=buf0+buf1
+    let freqB=parseInt(buf01,2)
+    let freq= ((freqB*32768/4)-225000)/1000000
+    freq=Math.round(freq*10)/10
+    if(parseInt(buf0,2)>>7==1){
+      is_ready=false
+    }else{
+      is_ready=true
+    }
+    if(parseInt(buf1,2)>>7==1){
+      is_stereo=false
+    }else{
+      is_stereo=true
+    }
+    signal_adc_level=parseInt(String(temp[3]),2)>>4
+  }
+  function update() {
+    buf = pins.createBuffer(5)
+    let cmd = ''
+    if (band == 'US') {
+      if (frequency < range_US[0]) {
+        frequency = range_US[0]
+      } else if (frequency > range_US[1]) {
+        frequency = range_US[1]
+      }
+    }
+    else {
+      if (frequency < range_JP[0]) {
+        frequency = range_JP[0]
+      } else if (frequency > range_JP[1]) {
+        frequency = range_JP[1]
+      }
+    }
+    let freqB = (frequency * 100000 + 225000) * 4 / 32768
+    let freqH = Math.abs(freqB) >> 8
+    let freqL = Math.abs(freqB) & 0xFF
+    if (mute_mode) {
+      cmd = '1'
+    } else {
+      cmd = '0'
+    }
+    if (search_mode) {
+      cmd += '1'
+    } else {
+      cmd += '0'
+    }
+    buf[0] = Math.abs(freqH)
+    buf[1] = freqL
+    if (search_direction == 1) {
+      cmd = '1'
+    } else {
+      cmd = '0'
+    }
+    if (search_adc_level == 10) {
+      cmd += '11'
+    } else if (search_adc_level == 7) {
+      cmd += '10'
+    } else if (search_adc_level == 5) {
+      cmd += '01'
+    } else {
+      cmd += '00'
+    }
+    cmd += '1'
+    if (stereo_mode) {
+      cmd += '0'
+    } else {
+      cmd += '1'
+    }
+    cmd + '000'
+    buf[2]=parseInt(cmd,2)
+    cmd='0'
+    if (standby_mode){
+      cmd+='1'
+    }else{
+      cmd+='0'
+    }
+    if(band=='JP'){
+      cmd+='1'
+    }else{
+      cmd+='0'
+    }
+    cmd+='1'
+    if(soft_mute_mode){
+      cmd+='1'
+    }else{
+      cmd+='0'
+    }
+    if(high_cut_mode){
+      cmd+='1'
+    }else{
+      cmd+='0'
+    }
+    if(noise_cancel){
+      cmd+='1'
+    }else{
+      cmd+='0'
+    }
+    cmd+='0'
+    buf[3]=parseInt(cmd,2)
+    buf[4]=0
+    pins.i2cWriteBuffer(addr,buf)
+    read()
+  }
+}
